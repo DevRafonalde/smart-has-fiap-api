b8d0fff1e82e10ee106b6562024f0f4b89651225
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2026-04-17 22:26:30 -0300

    initial commit

---
+++ src/main/java/br/com/fiap/on/smartHas/SmartHasApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.on.smartHas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/on/smartHas/SmartHasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.on.smartHas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
