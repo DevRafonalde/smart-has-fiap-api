3a62765cd73b4a5e771083b478403e53516cf898
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2026-04-18 00:01:23 -0300

    Corrigida a formatação dos nomes completos e amigáveis dos usuários

--- src/main/java/br/com/fiap/on/smarthas/auth/internal/services/UsuarioService.java
+++ src/main/java/br/com/fiap/on/smarthas/auth/internal/services/UsuarioService.java
@@ -6,12 +6,12 @@ import br.com.fiap.on.smarthas.auth.internal.models.entities.dto.UsuarioPerfilDT
 import br.com.fiap.on.smarthas.auth.internal.models.entities.orm.PerfilORM;
 import br.com.fiap.on.smarthas.auth.internal.models.entities.orm.UsuarioORM;
 import br.com.fiap.on.smarthas.auth.internal.models.entities.orm.UsuarioPerfilORM;
-import br.com.fiap.on.smarthas.auth.internal.models.repositories.PerfilRepository;
 import br.com.fiap.on.smarthas.auth.internal.models.repositories.UsuarioPerfilRepository;
 import br.com.fiap.on.smarthas.auth.internal.models.repositories.UsuarioRepository;
 import br.com.fiap.on.smarthas.config.PasswordUtil;
 import br.com.fiap.on.smarthas.shared.exceptions.AtributoJaUtilizadoException;
 import br.com.fiap.on.smarthas.shared.exceptions.ElementoNaoEncontradoException;
+import br.com.fiap.on.smarthas.shared.utils.FormatarNomeMaiusculo;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
@@ -29,27 +29,15 @@ public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioPerfilRepository usuarioPerfilRepository;
-    private final PerfilRepository perfilRepository;
     private final ModelMapper mapper;
 
     public List<UsuarioDTO> listarTodos(Pageable pageable) {
         Page<UsuarioORM> usuarios = usuarioRepository.findAll(pageable);
 
-        List<UsuarioDTO> usuariosAmigaveis = usuarios.stream()
+        return usuarios.stream()
                 .filter(usuario -> usuario.getNomeAmigavel() != null && usuario.getAtivo())
                 .map(usuario -> mapper.map(usuario, UsuarioDTO.class))
                 .toList();
-
-        usuariosAmigaveis.forEach(
-                usuario -> usuario.setNomeAmigavel(
-                        usuario.getNomeAmigavel()
-                                .substring(0, 1)
-                                .toUpperCase()
-                                .concat(usuario.getNomeAmigavel().substring(1))
-                )
-        );
-
-        return usuariosAmigaveis;
     }
 
     public UsuarioPerfilDTO listarEspecifico(Integer id) {
@@ -97,6 +85,9 @@ public class UsuarioService {
 
         UsuarioORM usuarioRecebido = mapper.map(usuarioPerfilDTO.getUsuario(), UsuarioORM.class);
         usuarioRecebido.setSenhaUser(PasswordUtil.hashPassword(usuarioRecebido.getSenhaUser()));
+        usuarioRecebido.setNomeAmigavel(FormatarNomeMaiusculo.formatar(usuarioRecebido.getNomeAmigavel()));
+        usuarioRecebido.setNomeCompleto(FormatarNomeMaiusculo.formatar(usuarioRecebido.getNomeCompleto()));
+
         UsuarioORM usuarioCadastrado = usuarioRepository.save(usuarioRecebido);
 
         List<PerfilORM> perfis = usuarioPerfilDTO.getPerfisUsuario()
@@ -119,23 +110,25 @@ public class UsuarioService {
     }
 
     public UsuarioPerfilDTO editar(UsuarioPerfilDTO modeloCadastroUsuarioPerfil) {
-        UsuarioORM usuarioMexido = mapper.map(modeloCadastroUsuarioPerfil.getUsuario(), UsuarioORM.class);
+        UsuarioORM usuarioRecebido = mapper.map(modeloCadastroUsuarioPerfil.getUsuario(), UsuarioORM.class);
 
-        UsuarioORM usuarioBanco = usuarioRepository.findById(usuarioMexido.getId())
+        UsuarioORM usuarioBanco = usuarioRepository.findById(usuarioRecebido.getId())
                 .orElseThrow(() -> new ElementoNaoEncontradoException("Usuário não encontrado no banco de dados"));
 
-        if (!usuarioMexido.getNomeUser().equalsIgnoreCase(usuarioBanco.getNomeUser())) {
-            UsuarioORM usuarioExistente = usuarioRepository.findByNomeUser(usuarioMexido.getNomeUser());
+        if (!usuarioRecebido.getNomeUser().equalsIgnoreCase(usuarioBanco.getNomeUser())) {
+            UsuarioORM usuarioExistente = usuarioRepository.findByNomeUser(usuarioRecebido.getNomeUser());
             if (Objects.nonNull(usuarioExistente)) {
                 throw new AtributoJaUtilizadoException("Nome de usuário já está sendo utilizado");
             }
         }
 
-        usuarioMexido.setSenhaUser(usuarioBanco.getSenhaUser());
+        usuarioRecebido.setSenhaUser(usuarioBanco.getSenhaUser());
+        usuarioRecebido.setNomeAmigavel(FormatarNomeMaiusculo.formatar(usuarioRecebido.getNomeAmigavel()));
+        usuarioRecebido.setNomeCompleto(FormatarNomeMaiusculo.formatar(usuarioRecebido.getNomeCompleto()));
 
-        UsuarioORM usuarioSalvo = usuarioRepository.save(usuarioMexido);
+        UsuarioORM usuarioSalvo = usuarioRepository.save(usuarioRecebido);
 
-        List<UsuarioPerfilORM> registrosExistentes = usuarioPerfilRepository.findByUsuario(usuarioMexido);
+        List<UsuarioPerfilORM> registrosExistentes = usuarioPerfilRepository.findByUsuario(usuarioRecebido);
         usuarioPerfilRepository.deleteAll(registrosExistentes);
 
         List<PerfilORM> perfis = modeloCadastroUsuarioPerfil.getPerfisUsuario()
@@ -147,7 +140,7 @@ public class UsuarioService {
 
         for (PerfilORM perfil : perfis) {
             UsuarioPerfilORM usuarioPerfil = new UsuarioPerfilORM();
-            usuarioPerfil.setUsuario(usuarioMexido);
+            usuarioPerfil.setUsuario(usuarioRecebido);
             usuarioPerfil.setDataHora(LocalDateTime.now());
             usuarioPerfil.setPerfil(perfil);
             usuarioPerfilRepository.save(usuarioPerfil);
@@ -155,26 +148,6 @@ public class UsuarioService {
         }
 
         return new UsuarioPerfilDTO(mapper.map(usuarioSalvo, UsuarioDTO.class), perfisDto);
-    }
-
-    public void criarUsuarioAdminPadrao() {
-        if (Objects.isNull(usuarioRepository.findByNomeUser("admin"))) {
-            UsuarioORM admin = new UsuarioORM();
-            admin.setNomeCompleto("Administrador");
-            admin.setNomeAmigavel("Administrador");
-            admin.setNomeUser("admin");
-            admin.setSenhaUser(PasswordUtil.hashPassword("123456"));
-
-            UsuarioORM usuarioAdmin = usuarioRepository.save(admin);
-
-            PerfilORM perfilAdmin = perfilRepository.findByMnemonico("admin-sistema");
-
-            UsuarioPerfilORM usuarioPerfilAdmin = new UsuarioPerfilORM();
-            usuarioPerfilAdmin.setUsuario(usuarioAdmin);
-            usuarioPerfilAdmin.setPerfil(perfilAdmin);
-
-            usuarioPerfilRepository.save(usuarioPerfilAdmin);
-        }
     }
 
     public UsuarioORM autenticar(String nomeUser, String senha) {
