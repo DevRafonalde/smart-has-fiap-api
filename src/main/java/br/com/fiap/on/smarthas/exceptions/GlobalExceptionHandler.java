0b4760d27cf49f91fc667c8b9388632387168cd8
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2026-05-10 11:09:00 -0300

    fix: corrigido o tratamento de exceções

--- src/main/java/br/com/fiap/on/smarthas/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/on/smarthas/exceptions/GlobalExceptionHandler.java
@@ -12,11 +12,40 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AcessoNaoAutorizadoException.class)
-    public ResponseEntity<Map<String, Object>> handleUnauthorized(AcessoNaoAutorizadoException ex) {
+    public ResponseEntity<Map<String, Object>> handleUNAUTHORIZED(AcessoNaoAutorizadoException ex) {
+        return montarRetorno(ex, HttpStatus.UNAUTHORIZED);
+    }
+
+    @ExceptionHandler(AtributoJaUtilizadoException.class)
+    public ResponseEntity<Map<String, Object>> handleCONFLICTAtributo(AtributoJaUtilizadoException ex) {
+        return montarRetorno(ex, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(CondicaoInsatisfeitaException.class)
+    public ResponseEntity<Map<String, Object>> handleFORBIDDEN(CondicaoInsatisfeitaException ex) {
+        return montarRetorno(ex, HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(CPFInvalidoException.class)
+    public ResponseEntity<Map<String, Object>> handleBAD_REQUESTCPF(CPFInvalidoException ex) {
+        return montarRetorno(ex, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ElementoExistenteException.class)
+    public ResponseEntity<Map<String, Object>> handleCONFLICTElemento(ElementoExistenteException ex) {
+        return montarRetorno(ex, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(ElementoNaoEncontradoException.class)
+    public ResponseEntity<Map<String, Object>> handleNOT_FOUNDNaoEncontrado(ElementoNaoEncontradoException ex) {
+        return montarRetorno(ex, HttpStatus.NOT_FOUND);
+    }
+
+    private ResponseEntity<Map<String, Object>> montarRetorno(RuntimeException ex, HttpStatus status) {
         Map<String, Object> body = new HashMap<>();
         body.put("error", ex.getMessage());
-        body.put("status", HttpStatus.UNAUTHORIZED.value());
+        body.put("status", status.value());
         body.put("timestamp", LocalDateTime.now());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
+        return ResponseEntity.status(status).body(body);
     }
 }
